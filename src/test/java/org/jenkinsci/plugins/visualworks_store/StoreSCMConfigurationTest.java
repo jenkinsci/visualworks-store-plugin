240ae0f70abdf4c3a41e7139ae9e00296df16c68
Randy Coulman <rcoulman@gmail.com>
2020-09-16 20:40:14 -0700

    🚨 Migrate away from deprecated HudsonTestCase
    It has been replaced by `JenkinsRule` instead.  Also, migrate from its deprecated `hudson` field to `jenkins`.

--- src/test/java/org/jenkinsci/plugins/visualworks_store/StoreSCMConfigurationTest.java
+++ src/test/java/org/jenkinsci/plugins/visualworks_store/StoreSCMConfigurationTest.java
@@ -25,19 +25,17 @@
 package org.jenkinsci.plugins.visualworks_store;
 
 import hudson.model.FreeStyleProject;
-import org.jvnet.hudson.test.HudsonTestCase;
+import org.jvnet.hudson.test.JenkinsRule;
 
 import java.util.Arrays;
 import java.util.List;
 
 //@Ignore("Too slow")
-public class StoreSCMConfigurationTest extends HudsonTestCase {
+public class StoreSCMConfigurationTest extends JenkinsRule {
     public void testGlobalConfigurationRoundtrip() throws Exception {
-        StoreSCM.DescriptorImpl descriptor =
-                hudson.getDescriptorByType(StoreSCM.DescriptorImpl.class);
+        StoreSCM.DescriptorImpl descriptor = jenkins.getDescriptorByType(StoreSCM.DescriptorImpl.class);
 
-        descriptor.setStoreScripts(
-                new StoreScript("7.7.1", "/path/to/script-7.7.1"),
+        descriptor.setStoreScripts(new StoreScript("7.7.1", "/path/to/script-7.7.1"),
                 new StoreScript("7.9.1", "/path/to/script-7.9.1"));
 
         submit(createWebClient().goTo("configure").getFormByName("config"));
@@ -45,72 +43,54 @@ public class StoreSCMConfigurationTest extends HudsonTestCase {
         StoreScript[] scripts = descriptor.getStoreScripts();
         assertEquals("installation count", 2, scripts.length);
         assertEquals("first script name", "7.7.1", scripts[0].getName());
-        assertEquals("first script path", "/path/to/script-7.7.1",
-                scripts[0].getPath());
+        assertEquals("first script path", "/path/to/script-7.7.1", scripts[0].getPath());
         assertEquals("second script name", "7.9.1", scripts[1].getName());
-        assertEquals("second script path", "/path/to/script-7.9.1",
-                scripts[1].getPath());
+        assertEquals("second script path", "/path/to/script-7.9.1", scripts[1].getPath());
     }
 
     public void testBasicConfigurationRoundtrip() throws Exception {
-        StoreSCM.DescriptorImpl descriptor =
-                hudson.getDescriptorByType(StoreSCM.DescriptorImpl.class);
+        StoreSCM.DescriptorImpl descriptor = jenkins.getDescriptorByType(StoreSCM.DescriptorImpl.class);
         descriptor.setStoreScripts(new StoreScript("theScript", "path"));
 
         List<PundleSpec> pundleSpecs = onePundle();
-        StoreSCM scm = new StoreSCM("theScript", "Repo", pundleSpecs, "\\d+",
-                "Integrated", false, "");
+        StoreSCM scm = new StoreSCM("theScript", "Repo", pundleSpecs, "\\d+", "Integrated", false, "");
         StoreSCM loaded = doRoundtripConfiguration(scm);
 
         assertEquals("script name", "theScript", loaded.getScriptName());
         assertEquals("repositoryName", "Repo", loaded.getRepositoryName());
         assertEquals("pundles", pundleSpecs, loaded.getPundles());
         assertEquals("versionRegex", "\\d+", loaded.getVersionRegex());
-        assertEquals("minimumBlessingLevel", "Integrated",
-                loaded.getMinimumBlessingLevel());
-        assertFalse("generateParcelBuilderInputFile",
-                loaded.isGenerateParcelBuilderInputFile());
-        assertEquals("parcelBuilderInputFilename", "",
-                loaded.getParcelBuilderInputFilename());
+        assertEquals("minimumBlessingLevel", "Integrated", loaded.getMinimumBlessingLevel());
+        assertFalse("generateParcelBuilderInputFile", loaded.isGenerateParcelBuilderInputFile());
+        assertEquals("parcelBuilderInputFilename", "", loaded.getParcelBuilderInputFilename());
     }
 
-    public void testConfigurationRoundtripWithMultiplePundles()
-            throws Exception {
-        StoreSCM.DescriptorImpl descriptor =
-                hudson.getDescriptorByType(StoreSCM.DescriptorImpl.class);
+    public void testConfigurationRoundtripWithMultiplePundles() throws Exception {
+        StoreSCM.DescriptorImpl descriptor = jenkins.getDescriptorByType(StoreSCM.DescriptorImpl.class);
         descriptor.setStoreScripts(new StoreScript("theScript", "path"));
 
-        List<PundleSpec> pundleSpecs = Arrays.asList(
-                new PundleSpec(PundleType.PACKAGE, "SomePackage"),
+        List<PundleSpec> pundleSpecs = Arrays.asList(new PundleSpec(PundleType.PACKAGE, "SomePackage"),
                 new PundleSpec(PundleType.BUNDLE, "SomeBundle"));
-        StoreSCM scm = new StoreSCM("theScript", "Repo", pundleSpecs, "\\d+",
-                "Integrated", false, "");
+        StoreSCM scm = new StoreSCM("theScript", "Repo", pundleSpecs, "\\d+", "Integrated", false, "");
         StoreSCM loaded = doRoundtripConfiguration(scm);
 
         assertEquals("pundles", pundleSpecs, loaded.getPundles());
     }
 
-    public void testConfigurationRoundtripWithParcelBuilderFile()
-            throws Exception {
-        StoreSCM scm = new StoreSCM("script", "Repo", onePundle(), "\\d+",
-                "Integrated", true, "theFilename");
+    public void testConfigurationRoundtripWithParcelBuilderFile() throws Exception {
+        StoreSCM scm = new StoreSCM("script", "Repo", onePundle(), "\\d+", "Integrated", true, "theFilename");
         StoreSCM loaded = doRoundtripConfiguration(scm);
 
-        assertTrue("generateParcelBuilderInputFile",
-                loaded.isGenerateParcelBuilderInputFile());
-        assertEquals("parcelBuilderInputFilename", "theFilename",
-                loaded.getParcelBuilderInputFilename());
+        assertTrue("generateParcelBuilderInputFile", loaded.isGenerateParcelBuilderInputFile());
+        assertEquals("parcelBuilderInputFilename", "theFilename", loaded.getParcelBuilderInputFilename());
     }
 
     public void testLookupStoreScript() {
-        StoreSCM.DescriptorImpl descriptor =
-                hudson.getDescriptorByType(StoreSCM.DescriptorImpl.class);
+        StoreSCM.DescriptorImpl descriptor = jenkins.getDescriptorByType(StoreSCM.DescriptorImpl.class);
         final StoreScript script = new StoreScript("otherScript", "otherPath");
-        descriptor.setStoreScripts(new StoreScript("theScript", "path"),
-                script);
+        descriptor.setStoreScripts(new StoreScript("theScript", "path"), script);
 
-        StoreSCM scm = new StoreSCM("otherScript", "Repo", onePundle(),
-                "\\d+", "Development", false, "");
+        StoreSCM scm = new StoreSCM("otherScript", "Repo", onePundle(), "\\d+", "Development", false, "");
 
         assertEquals(script, scm.getStoreScript());
     }
@@ -125,7 +105,6 @@ public class StoreSCMConfigurationTest extends HudsonTestCase {
     }
 
     private List<PundleSpec> onePundle() {
-        return Arrays.asList(
-                new PundleSpec(PundleType.PACKAGE, "SomePackage"));
+        return Arrays.asList(new PundleSpec(PundleType.PACKAGE, "SomePackage"));
     }
 }
